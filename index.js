3de0d248749c83e4a61cc81136f227cd3dee7742
morswin22 <xmorswinx@gmail.com>
2020-01-27 19:33:23 +0100

    :sparkles: added mongodb connection and simple models

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const graphqlHTTP = require('express-graphql')
 const gql = require('graphql-tag')
 const { buildASTSchema } = require('graphql')
+const mongoose = require('mongoose')
+const { PubSub } = require('graphql-subscriptions')
+const models = require('./models')
+
+const { mongoURI: db } = process.env
+const pubsub = new PubSub()
+
+const context = {
+  models,
+  pubsub
+}
+
+mongoose
+  .connect(db, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.error(error))
 
 const app = express()
 app.use(cors())
 
 const schema = buildASTSchema(gql`
   type Query {
-    hello: String
+    hello(name: String): String
   }
 `)
 
-const rootValue = {
-  hello: () => 'Hello, world'
-}
-
-app.use('/graphql', graphqlHTTP({ schema, rootValue }))
+app.use('/graphql', graphqlHTTP({ schema, context }))
 
 const port = process.env.PORT || 4000
 app.listen(port)
