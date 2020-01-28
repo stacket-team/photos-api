3cd5f45675a506d2bf8ea7fe9ed95b3d5ad065f9
morswin22 <xmorswinx@gmail.com>
2020-01-28 19:35:53 +0100

    :sparkles: added react directory

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
-const { ApolloServer, PubSub } = require('apollo-server')
+const express = require('express')
+const path = require('path')
+const { ApolloServer, PubSub } = require('apollo-server-express')
 const mongoose = require('mongoose')
 const { models } = require('./models')
 const { schema } = require('./schema')
 
-const { mongoURI: db } = process.env
+const { NODE_ENV: env, MONGO_URI: db } = process.env
+
 const pubsub = new PubSub()
 
 const context = {
@@ -18,11 +21,29 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log(`MongoDB is connected on ${db}`))
   .catch(error => console.error(error))
 
 const server = new ApolloServer({ schema, context })
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
+const app = express()
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+if (env === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+  })
+} 
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Express is working on https://localhost:${port}/`)
+  console.log(`Graphql endpoint: https://localhost:${port}/graphql`)
+  if (env === 'production') {
+    console.log(`Dashboard: https://localhost:${port}/`);
+  } else {
+    console.log(`Dashboard: https://localhost:3000/`)
+  }
 })
