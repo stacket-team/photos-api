9297572fa8d5ca2174a00194a86d089c84a5ea8a
morswin22 <xmorswinx@gmail.com>
2020-02-23 12:22:41 +0100

    :bug: fixed user authorization

--- client/src/index.js
+++ client/src/index.js
@@ -5,21 +5,52 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { onError } from 'apollo-link-error';	
+import { ApolloLink, Observable } from 'apollo-link';
 
-class Authorization {
-  get headers() {
-    const token = localStorage.getItem('token');
-    return { authorization: token ? `Bearer ${token}` : '' }
-  }
-}
-
-const authorization = new Authorization();
+const request = (operation) => {
+  const token = localStorage.getItem('token');
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  });
+};
 
 const client = new ApolloClient({
-  link: createUploadLink({
-    uri: `${ process.env.REACT_APP_BACKEND_BASE }/graphql`,
-    headers: authorization.headers
-  }),
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new ApolloLink((operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(oper => request(oper))
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+    ),
+    createUploadLink({
+      uri: `${ process.env.REACT_APP_BACKEND_BASE }/graphql`
+    })
+  ]),
   cache: new InMemoryCache()
 });
 
